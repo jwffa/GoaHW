5ac404b87753ca9173f6b43befba82e2ca0714c7
jwffa <146485313+jwffa@users.noreply.github.com>
2024-03-15 16:33:08 +0400

    Add files via upload

---
+++ kahoot.py
@@ -0,0 +1,108 @@
+# for i in range(-100, -90, -1):
+#     print(i)
+
+
+# ----------------------------------------
+
+
+# result = [0, 1, 0, 0, 10]
+# for i in result:
+#     if i == 0:
+#         result.remove(i)
+# print(result)
+
+
+# ----------------------------------------
+
+# result = 'GoaBest'
+# for i in range(len(result)):
+#     print("Goa is the best")
+
+
+# ----------------------------------------
+
+# my_set = {'barcelona', 0, 'kvara', False, 'True', True, 2, 1}
+# print(my_set)
+
+
+# ----------------------------------------
+
+
+# number_1 = 10
+# number_2 = 15
+
+# while number_1 < number_2:
+#     number_1 += 1
+#     print('GOA')
+
+
+#     number_2 += 1
+
+
+
+# ----------------------------------------
+
+
+# my_text = '1 1 1 1'
+# print(len(my_text))
+
+
+# ----------------------------------------
+
+
+# count = 0
+
+# name = 'academy Goa'
+# name1 = 'academy goa'
+
+# for i in name:
+#     if i not in name1:
+#         count += 1
+# print(count)
+
+
+
+# ----------------------------------------
+
+
+# num_1 = 7
+# num_2 = 10
+
+# if(num_1 < num_2){
+#     print('Goa')
+# }else{
+#     print('False')
+# }
+
+
+# ----------------------------------------
+
+
+# my_dict = {
+#     "username": 'Nika',
+#     'email': 'keshka111@gmail.com',
+#     'pass': 'i love goa'
+# }
+# del my_dict['email']
+# my_dict.update({'age': 23})
+# print(my_dict)
+
+
+# ----------------------------------------
+
+
+# my_arr = [1, 2, 3, 4, 5]
+
+# def my_func(arr):
+#     for i in arr:
+#         if i in my_arr:
+#             print("It's in")
+# my_func([10, 102, 1001, '1', 3021])
+
+# def my_func1(arr1):
+#     my_func(arr1)
+
+# my_func1([1, 2, 3, 4, 5, 1])
+
+
+# ----------------------------------------
